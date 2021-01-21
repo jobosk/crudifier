b2d28c05c37f85219dfe3047e46ede2b8423c6d3
jbosca <jbosca@belikesoftware.com>
2021-01-21 15:01:35 +0100

    Refactorizacion.

--- src/main/java/com/jobosk/crudifier/service/IndexedCrudService.java
+++ src/main/java/com/jobosk/crudifier/service/IndexedCrudService.java
@@ -5,18 +5,18 @@ import com.jobosk.crudifier.repository.GenericRepository;
 import com.jobosk.crudifier.supplier.GenericSupplier;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public abstract class IndexedCrudService<Entity, Id, EntityIndex> extends EventCrudService<Entity, Id> {
+public abstract class IndexedCrudService<Entity, Id, Indexed> extends EventCrudService<Entity, Id> {
 
-    private final ElasticsearchRepository<EntityIndex, Id> elasticsearchRepository;
-    private final Class<EntityIndex> elasticIndexType;
+    private final ElasticsearchRepository<Indexed, Id> elasticsearchRepository;
+    private final Class<Indexed> elasticIndexType;
 
     public IndexedCrudService(
             final GenericRepository<Entity, Id> repository
             , final GenericSupplier<Entity> supplierCreate
             , final GenericSupplier<Entity> supplierUpdate
             , final GenericSupplier<Id> supplierDelete
-            , final ElasticsearchRepository<EntityIndex, Id> elasticsearchRepository
-            , final Class<EntityIndex> elasticIndexType
+            , final ElasticsearchRepository<Indexed, Id> elasticsearchRepository
+            , final Class<Indexed> elasticIndexType
     ) {
         super(repository, supplierCreate, supplierUpdate, supplierDelete);
         this.elasticsearchRepository = elasticsearchRepository;
@@ -25,8 +25,8 @@ public abstract class IndexedCrudService<Entity, Id, EntityIndex> extends EventC
 
     public IndexedCrudService(
             final GenericRepository<Entity, Id> repository
-            , final ElasticsearchRepository<EntityIndex, Id> elasticsearchRepository
-            , final Class<EntityIndex> elasticIndexType
+            , final ElasticsearchRepository<Indexed, Id> elasticsearchRepository
+            , final Class<Indexed> elasticIndexType
     ) {
         this(repository, null, null, null, elasticsearchRepository, elasticIndexType);
     }
@@ -35,13 +35,13 @@ public abstract class IndexedCrudService<Entity, Id, EntityIndex> extends EventC
     protected Entity update(final Entity obj) {
         final Entity result = super.update(obj);
         if (elasticsearchRepository != null && elasticIndexType != null) {
-            elasticsearchRepository.save(getEntityIndex(result));
+            elasticsearchRepository.save(getIndexedEntity(result));
         }
         return result;
     }
 
-    protected EntityIndex getEntityIndex(final Entity entity) {
-        EntityIndex result;
+    protected Indexed getIndexedEntity(final Entity entity) {
+        Indexed result;
         try {
             result = mapper.convertValue(entity, elasticIndexType);
         } catch (final Exception e) {
