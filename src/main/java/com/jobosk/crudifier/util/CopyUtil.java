229c07779041b5151a6f7298bd507be50ee1100a
jbosca <jbosca@belikesoftware.com>
2022-05-18 20:15:54 +0200

    Cambios en la actualizacion de colecciones para contemplar cuando el valor previo sea una coleccion de entidades con identificador.

--- src/main/java/com/jobosk/crudifier/util/CopyUtil.java
+++ src/main/java/com/jobosk/crudifier/util/CopyUtil.java
@@ -2,13 +2,15 @@ package com.jobosk.crudifier.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobosk.crudifier.entity.IHasIdentifier;
-import com.jobosk.crudifier.exception.CrudException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
 
@@ -22,47 +24,49 @@ public class CopyUtil {
 
   public static void copyProperties(final Object item, final Map<?, ?> props
       , final ObjectMapper mapper) {
+    copyProperties(item, props, new HashMap<>(), mapper);
+  }
+
+  public static void copyProperties(final Object item, final Map<?, ?> props
+      , final Map<Class<?>, Set<Object>> updatedIds, final ObjectMapper mapper) {
     final BeanWrapper itemWrapper = PropertyAccessorFactory.forBeanPropertyAccess(item);
     if (props != null) {
       props.entrySet().stream()
           .filter(e -> e.getKey() instanceof String && itemWrapper
               .isWritableProperty((String) e.getKey()))
-          .forEach(e -> setItemValue(itemWrapper, (String) e.getKey(), e.getValue(), mapper));
+          .forEach(e -> setItemValue(
+              itemWrapper
+              , (String) e.getKey()
+              , e.getValue()
+              , updatedIds
+              , mapper
+          ));
     }
   }
 
-  private static void setItemValue(final BeanWrapper itemWrapper, final String key,
-      final Object value
+  private static void setItemValue(final BeanWrapper itemWrapper, final String key
+      , final Object value, final Map<Class<?>, Set<Object>> updatedIds
       , final ObjectMapper mapper) {
-    itemWrapper.setPropertyValue(key, convertValue(
+    final PropertyDescriptor propertyDescriptor = itemWrapper.getPropertyDescriptor(key);
+    final Object previousValue = itemWrapper.getPropertyValue(key);
+    final Object convertedValue = convertValue(
         value
-        , itemWrapper.getPropertyDescriptor(key)
-        , itemWrapper.getPropertyValue(key)
-        , mapper
-    ));
-  }
-
-  private static <T> Object convertValue(final Object value
-      , final PropertyDescriptor propertyDescriptor, final T previousValue
-      , final ObjectMapper mapper) {
-    if (value == null) {
-      return null;
-    }
-    Object convertedValue = getValue(
-        propertyDescriptor.getPropertyType()
-        , value
+        , propertyDescriptor
         , previousValue
+        , updatedIds
         , mapper
     );
-    if (!(convertedValue instanceof Collection)) {
-      return convertedValue;
+    if (convertedValue instanceof Collection) {
+      itemWrapper.setPropertyValue(key, convertCollection(
+          (Class<?>) getItemType(propertyDescriptor)
+          , (Collection<?>) convertedValue
+          , (Collection<?>) previousValue
+          , updatedIds
+          , mapper
+      ));
+    } else {
+      itemWrapper.setPropertyValue(key, convertedValue);
     }
-    return convertCollection(
-        (Collection<?>) convertedValue
-        , (Class<?>) getItemType(propertyDescriptor)
-        , previousValue
-        , mapper
-    );
   }
 
   private static Type getItemType(final PropertyDescriptor propertyDescriptor) {
@@ -70,29 +74,47 @@ public class CopyUtil {
         .getActualTypeArguments()[0];
   }
 
-  private static <T> Collection<T> convertCollection(final Collection<?> convertedValue
-      , final Class<? extends T> itemType, final T previousValue, final ObjectMapper mapper) {
-    Collection<T> convertedCollection = new HashSet<>();
-    try {
-      for (final Object v : convertedValue) {
-        convertedCollection.add(getValue(itemType, v, previousValue, mapper));
-      }
-    } catch (final Exception e) {
-      throw new CrudException(e, "cannot_convert_collection");
+  private static <T> Object convertValue(final Object value
+      , final PropertyDescriptor propertyDescriptor, final T previousValue
+      , final Map<Class<?>, Set<Object>> updatedIds, final ObjectMapper mapper) {
+    final Class<?> valueType = propertyDescriptor.getPropertyType();
+    final Object result = mapper.convertValue(value, valueType);
+    if (previousValue != null && IHasIdentifier.class.isAssignableFrom(valueType)) {
+      return updatePreviousValue(
+          previousValue
+          , ((IHasIdentifier<?>) previousValue).getId()
+          , result
+          , updatedIds
+          , mapper
+      );
     }
-    return convertedCollection;
+    return result;
   }
 
-  private static <T> T getValue(final Class<? extends T> valueType, final Object item
-      , final T previousValue, final ObjectMapper mapper) {
-    if (previousValue != null && IHasIdentifier.class.isAssignableFrom(valueType)) {
-      final IHasIdentifier<?> identifiedType = (IHasIdentifier<?>) previousValue;
-      final Map<?, ?> properties = mapper.convertValue(item, Map.class);
-      if (isSameItem(identifiedType.getId(), properties.remove(PROPERTY_ID))) {
-        return updateValue(previousValue, properties, mapper);
-      }
+  private static <T> T updatePreviousValue(final T previousValue, final Object previousValueId
+      , final T currentValue, final Map<Class<?>, Set<Object>> updatedIds
+      , final ObjectMapper mapper) {
+    final Map<?, ?> properties = mapper.convertValue(currentValue, Map.class);
+    if (canUpdatePreviousItem(
+        previousValueId
+        , properties.remove(PROPERTY_ID)
+        , updatedIds
+        , previousValue.getClass()
+    )) {
+      updatedIds
+          .computeIfAbsent(previousValue.getClass(), k -> new HashSet<>())
+          .add(previousValueId);
+      copyProperties(previousValue, properties, updatedIds, mapper);
     }
-    return mapper.convertValue(item, valueType);
+    return previousValue;
+  }
+
+  private static boolean canUpdatePreviousItem(final Object previousId, final Object id
+      , final Map<Class<?>, Set<Object>> updatedIds, final Class<?> previousType) {
+    final boolean previouslyUpdated = updatedIds
+        .getOrDefault(previousType, new HashSet<>())
+        .contains(previousId);
+    return isSameItem(previousId, id) && !previouslyUpdated;
   }
 
   private static boolean isSameItem(final Object v1, final Object v2) {
@@ -102,9 +124,31 @@ public class CopyUtil {
     return String.valueOf(v1).equals(String.valueOf(v2));
   }
 
-  private static <T> T updateValue(final T value, final Map<?, ?> properties
+  private static <T> Collection<T> convertCollection(final Class<? extends T> itemType
+      , final Collection<?> convertedValues, final Collection<? extends T> previousValues
+      , final Map<Class<?>, Set<Object>> updatedIds, final ObjectMapper mapper) {
+    Collection<T> convertedCollection = new HashSet<>();
+    for (final Object v : convertedValues) {
+      convertedCollection.add(mapper.convertValue(v, itemType));
+    }
+    if (IHasIdentifier.class.isAssignableFrom(itemType)) {
+      return updatedPreviousValues(convertedCollection, previousValues, updatedIds, mapper);
+    }
+    return convertedCollection;
+  }
+
+  private static <T> Collection<T> updatedPreviousValues(final Collection<T> values
+      , final Collection<? extends T> previousValues, final Map<Class<?>, Set<Object>> updatedIds
       , final ObjectMapper mapper) {
-    copyProperties(value, properties, mapper);
-    return value;
+    return previousValues.stream()
+        .map(pv -> {
+          final IHasIdentifier<?> previousValue = (IHasIdentifier<?>) pv;
+          return values.stream()
+              .filter(v -> previousValue.getId().equals(((IHasIdentifier<?>) v).getId()))
+              .findFirst()
+              .map(v -> updatePreviousValue(pv, previousValue.getId(), v, updatedIds, mapper))
+              .orElse(pv);
+        })
+        .collect(Collectors.toSet());
   }
 }
