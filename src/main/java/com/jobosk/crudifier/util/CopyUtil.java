1cf7b37b64c7667a1fd9664a3cca51ef9bac022c
jbosca <jbosca@belikesoftware.com>
2022-05-18 17:42:17 +0200

    Corregida la conversion de valores en el copiado de propiedades

--- src/main/java/com/jobosk/crudifier/util/CopyUtil.java
+++ src/main/java/com/jobosk/crudifier/util/CopyUtil.java
@@ -2,122 +2,109 @@ package com.jobosk.crudifier.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobosk.crudifier.entity.IHasIdentifier;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.PropertyAccessorFactory;
-
+import com.jobosk.crudifier.exception.CrudException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.PropertyAccessorFactory;
 
 public class CopyUtil {
 
-    private static final String PROPERTY_ID = "id";
+  private static final String PROPERTY_ID = "id";
 
-    public static void copyProperties(final Object item, final Map<?, ?> props, final ObjectMapper mapper) {
-        final BeanWrapper itemWrapper = PropertyAccessorFactory.forBeanPropertyAccess(item);
-        if (props != null) {
-            props.entrySet().stream()
-                    .filter(e -> e.getKey() instanceof String && itemWrapper.isWritableProperty((String) e.getKey()))
-                    .forEach(e -> setItemValue(itemWrapper, (String) e.getKey(), e.getValue(), mapper));
-        }
-    }
+  private CopyUtil() {
 
-    private static void setItemValue(final BeanWrapper itemWrapper, final String key, final Object value
-            , final ObjectMapper mapper) {
-        itemWrapper.setPropertyValue(key, convertValue(
-                value
-                , itemWrapper.getPropertyDescriptor(key)
-                , itemWrapper.getPropertyValue(key)
-                , mapper
-        ));
-    }
+  }
 
-    private static <ValueType> Object convertValue(final Object value, final PropertyDescriptor propertyDescriptor
-            , final ValueType previousValue, final ObjectMapper mapper) {
-        if (value == null) {
-            return null;
-        }
-        Object convertedValue = getValue(value, propertyDescriptor.getPropertyType(), previousValue, mapper);
-        if (!(convertedValue instanceof Collection)) {
-            return convertedValue;
-        }
-        return convertCollection(
-                (Collection<ValueType>) convertedValue
-                , (Class<ValueType>) getItemType(propertyDescriptor)
-                , previousValue
-                , mapper
-        );
+  public static void copyProperties(final Object item, final Map<?, ?> props
+      , final ObjectMapper mapper) {
+    final BeanWrapper itemWrapper = PropertyAccessorFactory.forBeanPropertyAccess(item);
+    if (props != null) {
+      props.entrySet().stream()
+          .filter(e -> e.getKey() instanceof String && itemWrapper
+              .isWritableProperty((String) e.getKey()))
+          .forEach(e -> setItemValue(itemWrapper, (String) e.getKey(), e.getValue(), mapper));
     }
+  }
 
-    private static Type getItemType(final PropertyDescriptor propertyDescriptor) {
-        return ((ParameterizedType) propertyDescriptor.getWriteMethod().getGenericParameterTypes()[0]).getActualTypeArguments()[0];
-    }
+  private static void setItemValue(final BeanWrapper itemWrapper, final String key,
+      final Object value
+      , final ObjectMapper mapper) {
+    itemWrapper.setPropertyValue(key, convertValue(
+        value
+        , itemWrapper.getPropertyDescriptor(key)
+        , itemWrapper.getPropertyValue(key)
+        , mapper
+    ));
+  }
 
-    private static <ValueType> Collection<ValueType> convertCollection(final Collection<ValueType> convertedValue
-            , final Class<ValueType> itemType, final Object previousValue, final ObjectMapper mapper) {
-        Collection<ValueType> convertedCollection;
-        try {
-            convertedCollection = convertedValue.getClass().getDeclaredConstructor().newInstance();
-            for (final Object v : convertedValue) {
-                convertedCollection.add(getValue(v, itemType, previousValue, mapper));
-            }
-        } catch (final Exception e) {
-            convertedCollection = null;
-        }
-        return convertedCollection;
+  private static <T> Object convertValue(final Object value
+      , final PropertyDescriptor propertyDescriptor, final T previousValue
+      , final ObjectMapper mapper) {
+    if (value == null) {
+      return null;
     }
+    Object convertedValue = getValue(
+        propertyDescriptor.getPropertyType()
+        , value
+        , previousValue
+        , mapper
+    );
+    if (!(convertedValue instanceof Collection)) {
+      return convertedValue;
+    }
+    return convertCollection(
+        (Collection<?>) convertedValue
+        , (Class<?>) getItemType(propertyDescriptor)
+        , previousValue
+        , mapper
+    );
+  }
 
-    private static <ValueType> ValueType getValue(final Object item, final Class<ValueType> valueType
-            , final Object previousValue, final ObjectMapper mapper) {
-        //if (isValidJsonObject(previousValue)) {
-        if (IHasIdentifier.class.isAssignableFrom(valueType)) {
-            final Map<?, ?> properties = mapper.convertValue(item, Map.class);
-            //final Map<?, ?> previousProperties = mapper.convertValue(previousValue, Map.class);
-            if (isSameItem(
-                    //previousProperties.get(PROPERTY_ID)
-                    ((IHasIdentifier<?>) valueType.cast(previousValue)).getId()
-                    , properties.remove(PROPERTY_ID)
-            )) {
-                return updateValue(valueType.cast(previousValue), properties, mapper);
-            }
-        }
-        return mapper.convertValue(item, valueType);
-    }
+  private static Type getItemType(final PropertyDescriptor propertyDescriptor) {
+    return ((ParameterizedType) propertyDescriptor.getWriteMethod().getGenericParameterTypes()[0])
+        .getActualTypeArguments()[0];
+  }
 
-    /*
-    private static boolean isValidJsonObject(final Object value) {
-        try {
-            final JsonParser parser = new JsonFactory().createParser(value.toString());
-            do {
-                parser.nextToken();
-            } while (!parser.isClosed());
-        } catch (final Exception e) {
-            return false;
-        }
-        return true;
+  private static <T> Collection<T> convertCollection(final Collection<?> convertedValue
+      , final Class<? extends T> itemType, final T previousValue, final ObjectMapper mapper) {
+    Collection<T> convertedCollection = new HashSet<>();
+    try {
+      for (final Object v : convertedValue) {
+        convertedCollection.add(getValue(itemType, v, previousValue, mapper));
+      }
+    } catch (final Exception e) {
+      throw new CrudException(e, "cannot_convert_collection");
     }
-    */
+    return convertedCollection;
+  }
 
-    private static boolean isSameItem(final Object v1, final Object v2) {
-        if (v1 == null || v2 == null) {
-            return false;
-        }
-        return String.valueOf(v1).equals(String.valueOf(v2));
+  private static <T> T getValue(final Class<? extends T> valueType, final Object item
+      , final T previousValue, final ObjectMapper mapper) {
+    if (previousValue != null && IHasIdentifier.class.isAssignableFrom(valueType)) {
+      final IHasIdentifier<?> identifiedType = (IHasIdentifier<?>) previousValue;
+      final Map<?, ?> properties = mapper.convertValue(item, Map.class);
+      if (isSameItem(identifiedType.getId(), properties.remove(PROPERTY_ID))) {
+        return updateValue(previousValue, properties, mapper);
+      }
     }
+    return mapper.convertValue(item, valueType);
+  }
 
-    private static <ValueType> ValueType updateValue(final ValueType value, final Map<?, ?> properties
-            , final ObjectMapper mapper) {
-        copyProperties(value, properties, mapper);
-        return value;
+  private static boolean isSameItem(final Object v1, final Object v2) {
+    if (v1 == null || v2 == null) {
+      return false;
     }
+    return String.valueOf(v1).equals(String.valueOf(v2));
+  }
 
-    /*
-    public static void copyProperties(final Object src, final Object trg, final Iterable<String> props) {
-        final BeanWrapper srcWrap = PropertyAccessorFactory.forBeanPropertyAccess(src);
-        final BeanWrapper trgWrap = PropertyAccessorFactory.forBeanPropertyAccess(trg);
-        props.forEach(p -> trgWrap.setPropertyValue(p, srcWrap.getPropertyValue(p)));
-    }
-    */
+  private static <T> T updateValue(final T value, final Map<?, ?> properties
+      , final ObjectMapper mapper) {
+    copyProperties(value, properties, mapper);
+    return value;
+  }
 }
