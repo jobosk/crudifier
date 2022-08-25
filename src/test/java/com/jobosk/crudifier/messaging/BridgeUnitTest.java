6af4e920f4793e16f304291c3f4ca424217dae4b
jbosca <jbosca@belikesoftware.com>
2022-08-26 00:35:57 +0200

    Expanded message bridge testing

--- src/test/java/com/jobosk/crudifier/messaging/BridgeUnitTest.java
+++ src/test/java/com/jobosk/crudifier/messaging/BridgeUnitTest.java
@@ -1,8 +1,10 @@
 package com.jobosk.crudifier.messaging;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.Duration;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.FluxSink;
@@ -18,9 +20,27 @@ class BridgeUnitTest {
   }
 
   @Test
-  void publish_isOk() {
+  void publishOne_isOk() {
     final MessageBridge<String> bridge = new MessageBridge<>();
     bridge.pushMessage("test");
     assertEquals("test", bridge.getMessageSupplier().blockFirst(Duration.ofMillis(1000L)));
+  }
+
+  @Test
+  void publishMany_isOk() {
+
+    final MessageBridge<String> bridge = new MessageBridge<>();
+    bridge.pushMessage("test1");
+    bridge.pushMessage("test2");
+    bridge.pushMessage("test3");
+
+    final List<String> results = bridge.getMessageSupplier()
+        .bufferTimeout(3, Duration.ofSeconds(5))
+        .blockFirst();
+    assertNotNull(results);
+    assertEquals(3, results.size());
+    assertEquals("test1", results.get(0));
+    assertEquals("test2", results.get(1));
+    assertEquals("test3", results.get(2));
   }
 }
