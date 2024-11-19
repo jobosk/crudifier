e06e6b1fcca15a4aef96e82cae302c8cf39ef494
jobosk <jfbosca+github@gmail.com>
2024-11-19 17:05:10 +0100

    correcciones.

--- src/main/java/com/jobosk/crudifier/util/ModelUtil.java
+++ src/main/java/com/jobosk/crudifier/util/ModelUtil.java
@@ -101,52 +101,47 @@ public abstract class ModelUtil {
                 .collect(Collectors.toList());
     }
 
-    public static <T extends IHasIdentifier<UUID>, R> List<Object> setTransientField(final List<T> items, final Object newItems
-            , final String transientField, final Function<UUID, Optional<R>> getter, final BiConsumer<T, R> setter
-            , final Consumer<T> reflexiveAction, final Class<T> type, final Supplier<T> builder
-            , final ObjectMapper mapper) {
-        return setTransientField(items, newItems, transientField, getter, setter, reflexiveAction
+    public static <T extends IHasIdentifier<UUID>, R> List<Object> setTransientField(final List<T> currentItems
+            , final Collection<?> newItems, final String transientField, final Function<UUID, Optional<R>> getter
+            , final BiConsumer<T, R> setter, final Consumer<T> reflexiveAction, final Class<T> type
+            , final Supplier<T> builder, final ObjectMapper mapper) {
+        return setTransientField(currentItems, newItems, transientField, getter, setter, reflexiveAction
                 , null, null, null, type, builder, mapper);
     }
 
-    public static <T extends IHasIdentifier<UUID>, R> List<Object> setTransientField(final List<T> items, final Object newItems
-            , final String transientField, final Function<UUID, Optional<R>> getter, final BiConsumer<T, R> setter
-            , final Consumer<T> reflexiveAction, final String recursionField, final Function<T, List<T>> recursiveGetter
-            , final BiConsumer<T, T> recursiveSetter, final Class<T> type, final Supplier<T> builder
-            , final ObjectMapper mapper) {
+    public static <T extends IHasIdentifier<UUID>, R> List<Object> setTransientField(final List<T> currentItems
+            , final Collection<?> newItems, final String transientField, final Function<UUID, Optional<R>> getter
+            , final BiConsumer<T, R> setter, final Consumer<T> reflexiveAction, final String recursionField
+            , final Function<T, List<T>> recursiveGetter, final BiConsumer<T, T> recursiveSetter, final Class<T> type
+            , final Supplier<T> builder, final ObjectMapper mapper) {
         final List<Object> sameProductItems = new ArrayList<>();
-        final Map<UUID, T> currentItems = items.stream()
+        final Map<UUID, T> currentItemsById = currentItems.stream()
                 .collect(Collectors.toMap(
                         T::getId
                         , i -> i
                 ));
-        Optional.ofNullable(newItems)
-                .filter(Collection.class::isInstance)
-                .map(Collection.class::cast)
-                .ifPresent(collection -> {
-                    for (final Object element : collection) {
-                        if (element instanceof Map) {
-                            updateTransientFields(
-                                    (Map<String, Object>) element
-                                    , currentItems
-                                    , transientField
-                                    , getter
-                                    , setter
-                                    , reflexiveAction
-                                    , recursionField
-                                    , recursiveGetter
-                                    , recursiveSetter
-                                    , () -> sameProductItems.add(element)
-                                    , type
-                                    , builder
-                                    , mapper
-                            );
-                        } else {
-                            FormatUtil.getUUID(element)
-                                    .ifPresent(sameProductItems::add);
-                        }
-                    }
-                });
+        for (final Object newItem : newItems) {
+            if (newItem instanceof Map) {
+                updateTransientFields(
+                        (Map<String, Object>) newItem
+                        , currentItemsById
+                        , transientField
+                        , getter
+                        , setter
+                        , reflexiveAction
+                        , recursionField
+                        , recursiveGetter
+                        , recursiveSetter
+                        , () -> sameProductItems.add(newItem)
+                        , type
+                        , builder
+                        , mapper
+                );
+            } else {
+                FormatUtil.getUUID(newItem)
+                        .ifPresent(sameProductItems::add);
+            }
+        }
         return sameProductItems;
     }
 
@@ -172,22 +167,31 @@ public abstract class ModelUtil {
                 .ifPresentOrElse(
                         i -> {
                             if (recursionField != null && recursiveGetter != null && recursiveSetter != null) {
-                                final List<Object> sameProductChildren = setTransientField(
-                                        recursiveGetter.apply(i)
-                                        , map.remove(recursionField)
-                                        , transientField
-                                        , getter
-                                        , setter
-                                        , c -> recursiveSetter.accept(i, c)
-                                        , recursionField
-                                        , recursiveGetter
-                                        , recursiveSetter
-                                        , type
-                                        , builder
-                                        , mapper
-                                );
-                                if (!sameProductChildren.isEmpty()) {
-                                    map.put(recursionField, sameProductChildren);
+                                final Collection<?> children = Optional.ofNullable(map.remove(recursionField))
+                                        .filter(Collection.class::isInstance)
+                                        .map(Collection.class::cast)
+                                        .orElse(new ArrayList<>());
+                                final List<T> currentChildren = recursiveGetter.apply(i);
+                                if (children.isEmpty() && !currentChildren.isEmpty()) {
+                                    map.put(recursionField, children);
+                                } else {
+                                    final List<Object> sameProductChildren = setTransientField(
+                                            currentChildren
+                                            , children
+                                            , transientField
+                                            , getter
+                                            , setter
+                                            , c -> recursiveSetter.accept(i, c)
+                                            , recursionField
+                                            , recursiveGetter
+                                            , recursiveSetter
+                                            , type
+                                            , builder
+                                            , mapper
+                                    );
+                                    if (!sameProductChildren.isEmpty()) {
+                                        map.put(recursionField, sameProductChildren);
+                                    }
                                 }
                             }
                             copyProperties(i, map, mapper);
@@ -228,40 +232,81 @@ public abstract class ModelUtil {
             return;
         }
         final PropertyDescriptor propertyDescriptor = itemWrapper.getPropertyDescriptor(key);
-        Object convertedValue = getValue(value, propertyDescriptor.getPropertyType(), mapper);
-        if (convertedValue instanceof Collection) {
-            final Collection collectionValues = (Collection) convertedValue;
-            Optional.ofNullable(itemWrapper.getPropertyValue(key))
-                    .filter(Collection.class::isInstance)
-                    .map(Collection.class::cast)
-                    .filter(currentValues -> currentValues.size() == collectionValues.size())
-                    .ifPresentOrElse(
-                            currentValues -> {
-                                final Iterator<?> currentIterator = currentValues.iterator();
-                                final Iterator<?> collectionIterator = collectionValues.iterator();
-                                while (currentIterator.hasNext() && collectionIterator.hasNext()) {
-                                    final Object collectionItem = collectionIterator.next();
-                                    if (collectionItem instanceof Map) {
-                                        copyProperties(currentIterator.next(), (Map) collectionItem, mapper);
-                                    }
-                                }
-                            }
-                            , () -> {
-                                final Collection<Object> convertedCollection = convertCollection(
-                                        collectionValues
-                                        , (Class) getItemType(propertyDescriptor)
-                                        , mapper
-                                );
-                                itemWrapper.setPropertyValue(key, convertedCollection);
-                            }
+        getValue(value, propertyDescriptor.getPropertyType(), mapper)
+                .ifPresent(convertedValue -> {
+                    if (convertedValue instanceof Collection) {
+                        final Collection<?> notCopied = copyCollectionValues(
+                                (Collection<?>) convertedValue
+                                , itemWrapper.getPropertyValue(key)
+                                , mapper
+                        );
+                        if (!notCopied.isEmpty()) {
+                            final Collection<?> convertedCollection = convertCollection(
+                                    notCopied
+                                    , (Class) getItemType(propertyDescriptor)
+                                    , mapper
+                            );
+                            itemWrapper.setPropertyValue(key, convertedCollection);
+                        }
+                    } else {
+                        itemWrapper.setPropertyValue(key, convertedValue);
+                    }
+                });
+    }
+
+    private static Collection<?> copyCollectionValues(final Collection<?> collectionValues, final Object propertyValue
+            , final ObjectMapper mapper) {
+        final List<?> notCopied = new ArrayList<>(collectionValues);
+        Optional.ofNullable(propertyValue)
+                .filter(Collection.class::isInstance)
+                .map(Collection.class::cast)
+                .ifPresent(values -> {
+                    final Map<UUID, Object> previousValues = new HashMap<>();
+                    values.forEach(value -> getId(value, mapper)
+                            .ifPresent(id -> previousValues.put(id, value))
                     );
-        } else {
-            itemWrapper.setPropertyValue(key, convertedValue);
+                    values.clear();
+                    for (final Object collectionValue : collectionValues) {
+                        getItemAttributes(collectionValue, mapper)
+                                .flatMap(collectionItem -> FormatUtil.getUUID(collectionItem.get("id"))
+                                        .map(previousValues::get)
+                                        .map(currentValue -> {
+                                            copyProperties(currentValue, collectionItem, mapper);
+                                            return currentValue;
+                                        })
+                                )
+                                .ifPresent(value -> {
+                                    values.add(value);
+                                    notCopied.remove(collectionValue);
+                                });
+                    }
+                });
+        return notCopied;
+    }
+
+    private static Optional<Map> getItemAttributes(final Object item, final ObjectMapper mapper) {
+        return Optional.ofNullable(item)
+                .filter(Map.class::isInstance)
+                .map(Map.class::cast)
+                .or(() -> getValue(item, Map.class, mapper));
+    }
+
+    private static Optional<UUID> getId(final Object currentValue, final ObjectMapper mapper) {
+        try {
+            final Map<?, ?> map = mapper.convertValue(currentValue, Map.class);
+            return Optional.ofNullable(map.get("id"))
+                    .flatMap(FormatUtil::getUUID);
+        } catch (final Exception e) {
+            return Optional.empty();
         }
     }
 
-    private static <T> T getValue(final Object value, final Class<T> propertyType, final ObjectMapper mapper) {
-        return mapper.convertValue(value, propertyType);
+    private static <T> Optional<T> getValue(final Object value, final Class<T> propertyType, final ObjectMapper mapper) {
+        try {
+            return Optional.of(mapper.convertValue(value, propertyType));
+        } catch (final Exception e) {
+            return Optional.empty();
+        }
     }
 
     private static Type getItemType(final PropertyDescriptor propertyDescriptor) {
@@ -275,7 +320,8 @@ public abstract class ModelUtil {
         try {
             convertedCollection = convertedValue.getClass().getDeclaredConstructor().newInstance();
             for (final Object v : convertedValue) {
-                convertedCollection.add(getValue(v, itemType, mapper));
+                getValue(v, itemType, mapper)
+                        .ifPresent(convertedCollection::add);
             }
         } catch (final Exception e) {
             convertedCollection = null;
