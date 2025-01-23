96cf187c0c28ef5af6d3562b3fc0d74b1f8e0312
jobosk <jfbosca+github@gmail.com>
2025-01-23 15:52:14 +0100

    Correccion por coherencia (aunque no da error).

--- src/main/java/com/jobosk/crudifier/repository/GenericRepository.java
+++ src/main/java/com/jobosk/crudifier/repository/GenericRepository.java
@@ -2,6 +2,8 @@ package com.jobosk.crudifier.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface GenericRepository<Entity, Id> extends JpaRepository<Entity, Id>, JpaSpecificationExecutor<Entity> {
 }
